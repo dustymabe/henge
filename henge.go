ca38b4a736d86ce968a05d92212fe488ccbca4ac
Ratnadeep Debnath <rtnpro@gmail.com>
2016-05-25 13:39:12 +0530

    Started refactoring and restructuring code for a generic implementation.

--- henge.go
+++ henge.go
@@ -2,56 +2,23 @@ package main
 
 import (
 	"flag"
-	"os"
+	"fmt"
 
-	"github.com/rtnpro/henge/pkg/generate/dockercompose"
-
-	kapi "k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/api/unversioned"
-	"k8s.io/kubernetes/pkg/kubectl"
-	"k8s.io/kubernetes/pkg/runtime"
+	"github.com/rtnpro/henge/pkg/loaders/compose"
+	"github.com/rtnpro/henge/pkg/transformers"
 )
 
-func convertToVersion(objs []runtime.Object, version string) ([]runtime.Object, error) {
-	ret := []runtime.Object{}
-
-	for _, obj := range objs {
-
-		convertedObject, err := kapi.Scheme.ConvertToVersion(obj, version)
-		if err != nil {
-			return nil, err
-		}
-
-		ret = append(ret, convertedObject)
-	}
-
-	return ret, nil
-}
-
 func main() {
+	provider := flag.String("provider", "openshift", "Target provider")
 
 	flag.Parse()
 
-	template, err := dockercompose.Generate(flag.Args()[0:]...)
+	project, err = compose.Load(flag.Args()[0:]...)
+
 	if err != nil {
+		fmt.Println("Error: ", err)
 		return
 	}
 
-	var convErr error
-	template.Objects, convErr = convertToVersion(template.Objects, "v1")
-	if convErr != nil {
-		panic(convErr)
-	}
-
-	// make it List instead of Template
-	list := &kapi.List{Items: template.Objects}
-
-	p, _, err := kubectl.GetPrinter("yaml", "")
-	if err != nil {
-		panic(err)
-	}
-	version := unversioned.GroupVersion{Group: "", Version: "v1"}
-	p = kubectl.NewVersionedPrinter(p, kapi.Scheme, version)
-	p.PrintObj(list, os.Stdout)
-
+	transformers.Transform(provider, project)
 }
